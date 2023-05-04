5272e56e7af1db9226c740ecbc8131737cdf626d
GeoReece000 <geofreytegeret@gmail.com>
2023-05-04 11:58:29 +0300

    News fetch

--- src/components/News.js
+++ src/components/News.js
@@ -2,3 +2,12 @@ import React, { useState, useEffect } from "react";
 
 function News() {
   const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const url =
+      "https://newsapi.org/v2/top-headlines?country=us&apiKey=615ef18cc55246f1b0516267e5a55737";
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setNews(data.articles))
+      .catch((error) => console.log(error));
+  }, []);

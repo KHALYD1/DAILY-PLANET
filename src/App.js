4686d57cafedc35e3bcf944800358914c60178c1
GeoReece000 <geofreytegeret@gmail.com>
2023-05-04 11:56:54 +0300

    News

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Aboutus from './components/Aboutus';
-
+import News from './components/News';
 
 function App() {
   return (
     <div className="App" >
       <Navbar />
+      < News/>
       <header className="App-header">
       <Aboutus /> 
       </header>

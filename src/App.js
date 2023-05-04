a550b54f178a17955e3a3ced52885e5f4f07a8d5
GeoReece000 <geofreytegeret@gmail.com>
2023-05-04 12:33:50 +0300

    Hero section

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Aboutus from './components/Aboutus';
 import News from './components/News';
+import Hero from './components/Hero';
 
 function App() {
   return (
     <div className="App" >
       <Navbar />
+      < Hero />
       < News/>
       <header className="App-header">
       <Aboutus /> 
---
+++ src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className="hero">
+      <img
+        src="https://i.pinimg.com/originals/ae/72/3c/ae723cf261173198aca44c5125a2f220.jpg"
+        alt="Daily Planet"
+        className="hero-image"
+      />

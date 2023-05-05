6c3c98015133dfaecbc6c04258977de4f0848af3
git-savio1.domi <techsavvydominic3@gmail.com>
2023-05-05 08:50:33 +0300

    Implemented routing for About Us, News, and Footer pages

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
-import './App.css';
-import Search from './components/SearchButton'
-import { useState } from 'react';
-import SearchButton from './components/SearchButton';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import { useState } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import './App.css';
+
+import SearchButton from './components/SearchButton';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Aboutus from './components/Aboutus';
@@ -10,34 +14,48 @@ import News from './components/News';
 import Hero from './components/Hero';
 
 function App() {
-  const [searchResults, setSearchResults] = useState([])
+  const [searchResults, setSearchResults] = useState([]);
 
   const handleSearch = (data) => {
-    setSearchResults(data)
-  }
-  return (
-    <div className="App" >
-      <Navbar />
+    setSearchResults(data);
+  };
 
+  return (
+    <div className="App">
+      <Navbar />
       <SearchButton handleSearch={handleSearch} />
       <ul>
         {searchResults.map((result) => (
           <li key={result.id}>{result.title}</li>
         ))}
       </ul>
-
-      
-
-      < Hero />
-      < News/>
-
-
+      <Hero />
+      <News />
       <header className="App-header">
-      <Aboutus /> 
+        <Aboutus />
       </header>
       <Footer />
     </div>
   );
 }
+
+ReactDOM.render(
+  <App>
+  <BrowserRouter>
+      <Route exact path="/aboutus">
+        <Aboutus />
+      </Route>
+      <Route exact path="/news">
+        <News />
+      </Route>
+      <Route exact path="/footer">
+        <Footer />
+      </Route>
+      <Route exact path="/">
+      </Route>
+  </BrowserRouter>
+  </App>,
+  document.getElementById('root')
+);
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './navbar.css';
-
+ 
 function Navbar() {
   return (
     <nav>
-        <img 
-                            id='image'
-                            src='https://www.seekpng.com/png/detail/272-2720223_daily-planet-logo-1932-prototype-by-noahlc-superman.png' alt=''
-                     />
+      <img
+        id='image'
+        src='https://www.seekpng.com/png/detail/272-2720223_daily-planet-logo-1932-prototype-by-noahlc-superman.png' alt=''
+      />
       <ul>
         <li><a href='#home'>Home</a></li>
         <li><a href='#news'>News</a></li>

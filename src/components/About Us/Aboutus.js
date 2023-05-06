09aa1aa9634d124deb557391d7d30ea2d6903d93
Khalid <khaliddakar001@gmail.com>
2023-05-06 16:50:12 +0300

    changed logo

--- src/components/About Us/Aboutus.js
+++ src/components/About Us/Aboutus.js
@@ -1,26 +1,83 @@
 import React from 'react'
 import styled from 'styled-components';
 
+// const AboutUsContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   margin-top: 20px;
+//   background-color: silver;
+// `;
+
+// const Photo = styled.div`
+// margin-left: -500px;
+// margin-top: 20px;
+
+//   img {
+//     max-width: 100%;
+//   }
+  
+// `;
+
+
+// const Description = styled.div`
+// color: black;
+//   h2, h3, h4 {
+//     margin-bottom: 10px;
+//   }
+
+//   p {
+//     color: blue;
+//     margin: 0 0 10px 0;
+//   }
+// `;
+
+// function Aboutus() {
+//   return (
+//     <AboutUsContainer>
+//       <Photo>
+//         <img src="https://cdn.shopify.com/s/files/1/0066/0478/3687/products/lsd006-dailyplanetcc-281219-110210_1200x1200.jpg?v=1635142728" alt="" />
+//       </Photo>
+//       <Description>
+//         <h2>Khalid Abdi</h2>
+//         <p1>Senior Developer at The Daily Planet</p1>
+//         <p>Email @ khalyddyce@icloud.com</p>
+//         <h3>Dominic Juma</h3>
+//         <p1>Senior Developer at The Daily Planet</p1>
+//         <p>Email @ jumawafuladominic@gmail.com</p>
+//         <h4>Geofrey Tegeret</h4>
+//         <p1>Senior Developer at The Daily Planet</p1>
+//         <p>Email @ georeece000@gmail.com</p>
+//       </Description>
+//     </AboutUsContainer>
+//   );
+// }
+// const AboutUsContainer = styled.div`
+//   display: flex;
+//   justify-content: center;
+//   align-items: center;
+//   margin-top: 20px;
+// `;
 const AboutUsContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
   margin-top: 20px;
+  background-color: silver;
 `;
 
 const Photo = styled.div`
-margin-left: -200px;
-margin-top: 20px;
-
+  margin-right: 20px;
+  margin-left: -500px;
+  
   img {
     max-width: 100%;
   }
-  
 `;
 
-
 const Description = styled.div`
-color: black;
+  color: black;
+  
   h2, h3, h4 {
     margin-bottom: 10px;
   }
@@ -51,5 +108,6 @@ function Aboutus() {
     </AboutUsContainer>
   );
 }
+
 
 export default Aboutus;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,184 +1,4 @@
-// import React from 'react';
-// import styled from 'styled-components';
-// import { Link, NavLink } from 'react-router-dom';
-// import SearchButton from '../SearchButton/SearchButton';
 
-// const Nav = styled.nav`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   background-color: white;
-//   padding: 10px 20px;
-// `;
-
-// const Logo = styled.img`
-//   height: 100px;
-// `;
-
-// const NavMenu = styled.ul`
-//   display: flex;
-//   list-style: none;
-//   margin: 10px;
-//   padding: 50px;
-//   font-size: 30px;
-//   text-decoration: underline;
-//   justify-content: space-between;
-//   gap: 30px;
-// `;
-
-// const NavItem = styled.li`
-//   margin: 0 10px;
-// `;
-
-// const NavLinkStyled = styled(NavLink)`
-//   color: black;
-//   font-weight: bold;
-//   text-decoration: none;
-//   transition: background-color 0.2s ease, color 0.2s ease;
-
-//   &:hover {
-//     background-color: darkgrey;
-//     color: black;
-//   }
-
-//   &.active {
-//     background-color: darkgrey;
-//     color: black;
-//   }
-// `;
-
-// const SearchWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const SearchContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   padding:  9px;
-//   margin-right: 10px;
-// `;
-
-
-
-// function Navbar() {
-//   return (
-//     <Nav>
-//       <Logo
-//         src="https://pm1.narvii.com/8075/350acd527c36799a73c4b925dcc5cde50abf6096r1-2048-683v2_hq.jpg"
-//         alt=""
-//       />
-//       <SearchWrapper>
-//         <SearchContainer>
-//           <SearchButton handleSearch={(articles) => console.log(articles)} />
-//         </SearchContainer>
-//       </SearchWrapper>
-//       <NavMenu>
-//         <NavItem>
-//           <NavLinkStyled exact to="/hero">HOME</NavLinkStyled>
-//         </NavItem>
-//         <NavItem>
-//           <NavLinkStyled to="/news">NEWS</NavLinkStyled>
-//         </NavItem>
-//         <NavItem>
-//           <NavLinkStyled to="/aboutus">ABOUT US</NavLinkStyled>
-//         </NavItem>
-//       </NavMenu>
-//     </Nav>
-//   );
-// }
-
-// export default Navbar;
-
-// import React from 'react';
-// import styled from 'styled-components';
-// import { Link, NavLink } from 'react-router-dom';
-// import SearchButton from '../SearchButton/SearchButton';
-
-// const Nav = styled.nav`
-//   display: flex;
-//   justify-content: space-between;
-//   align-items: center;
-//   background-color: white;
-//   padding: 10px 20px;
-// `;
-
-// const Logo = styled.img`
-//   height: 100px;
-// `;
-
-// const NavMenu = styled.ul`
-//   display: flex;
-//   list-style: none;
-//   margin: 10px;
-//   padding: 50px;
-//   font-size: 30px;
-//   text-decoration: underline;
-//   justify-content: space-between;
-//   gap: 30px;
-// `;
-
-// const NavItem = styled.li`
-//   margin: 0 10px;
-// `;
-
-// const NavLinkStyled = styled(NavLink)`
-//   color: black;
-//   font-weight: bold;
-//   text-decoration: none;
-//   transition: background-color 0.2s ease, color 0.2s ease;
-
-//   &:hover {
-//     background-color: darkgrey;
-//     color: black;
-//   }
-
-//   &.active {
-//     background-color: darkgrey;
-//     color: black;
-//   }
-// `;
-
-// const SearchWrapper = styled.div`
-//   display: flex;
-//   align-items: center;
-// `;
-
-// const SearchContainer = styled.div`
-//   display: flex;
-//   align-items: center;
-//   padding:  9px;
-//   margin-right: 10px;
-// `;
-
-// function Navbar() {
-//   return (
-//     <Nav>
-//       <Logo
-//         src="https://pm1.narvii.com/8075/350acd527c36799a73c4b925dcc5cde50abf6096r1-2048-683v2_hq.jpg"
-//         alt=""
-//       />
-//       <SearchWrapper>
-//         <SearchContainer>
-//           <SearchButton handleSearch={(articles) => console.log(articles)} />
-//         </SearchContainer>
-//       </SearchWrapper>
-//       <NavMenu>
-//         <NavItem>
-//           <NavLinkStyled exact to="/hero">HOME</NavLinkStyled>
-//         </NavItem>
-//         <NavItem>
-//           <NavLinkStyled to="/news">NEWS</NavLinkStyled>
-//         </NavItem>
-//         <NavItem>
-//           <NavLinkStyled to="/aboutus">ABOUT US</NavLinkStyled>
-//         </NavItem>
-//       </NavMenu>
-//     </Nav>
-//   );
-// }
-
-// export default Navbar;
 
 import React from 'react';
 import styled from 'styled-components';
@@ -190,12 +10,12 @@ const Nav = styled.nav`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  background-color: white;
-  padding: 10px 20px;
+  background-color: grey;
+  padding: 10px 10px;
 `;
 
 const Logo = styled.img`
-  height: 100px;
+  height: 80px;
 `;
 
 const NavMenu = styled.ul`
@@ -211,6 +31,7 @@ const NavMenu = styled.ul`
 
 const NavItem = styled.li`
   margin: 0 10px;
+  
 `;
 
 const NavLinkStyled = styled(NavLink)`
@@ -246,7 +67,7 @@ function Navbar() {
   return (
     <Nav>
       <Logo
-        src="https://pm1.narvii.com/8075/350acd527c36799a73c4b925dcc5cde50abf6096r1-2048-683v2_hq.jpg"
+        src="https://dailyplanetdc.com/wp-content/uploads/2020/04/DailyPlanetDC-White-2.png"
         alt=""
       />
       <SearchWrapper>
